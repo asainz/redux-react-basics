a0c073a886515f89187c190288689846aed77996
Andres Sainz <andressainz@gmail.com>
2019-03-02 15:34:46 +0100

    add some markup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubtractClick = this.handleSubtractClick.bind(this);
+        this.handleSumClick = this.handleSumClick.bind(this);
+    }
+
+    handleSumClick() {}
+    handleSubtractClick() {}
+
     render() {
-        return <div className="App">Add random number</div>;
+        return (
+            <div className="App">
+                <h3>
+                    Starting number: <strong>1000</strong>
+                </h3>
+                <h1>
+                    Current sum: <strong>1000</strong>
+                </h1>
+                <div>
+                    <button onClick={this.handleSumClick}>Sum a random number</button>
+                </div>
+                <div>
+                    <button onClick={this.handleSubtractClick}>Subtract a random number</button>
+                </div>
+            </div>
+        );
     }
 }
 
