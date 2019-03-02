d80c544c302a07710669d536eb21dc6c590da2dd
Andres Sainz <andressainz@gmail.com>
2019-03-02 16:42:15 +0100

    redux - step 2 - move things to a higher component as pass as props

--- src/App.js
+++ src/App.js
@@ -2,21 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 import Calculator from './Calculator';
 
+import store from './store';
+import rand from './rand';
+
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.handleSubtractClick = this.handleSubtractClick.bind(this);
         this.handleSumClick = this.handleSumClick.bind(this);
+
+        store.subscribe(() => {
+            window.console.log('state', store.getState().calculator);
+            this.forceUpdate();
+        });
     }
 
-    handleSumClick() {}
-    handleSubtractClick() {}
+    handleSumClick() {
+        store.dispatch({
+            type: 'SUM',
+            value: rand(1, 100),
+        });
+    }
+    handleSubtractClick() {
+        store.dispatch({
+            type: 'SUBTRACT',
+            value: rand(1, 100),
+        });
+    }
 
     render() {
+        const calculator = store.getState().calculator;
         return (
             <div className="App">
-                <Calculator />
+                <Calculator
+                    handleSumClick={this.handleSumClick}
+                    handleSubtractClick={this.handleSubtractClick}
+                    calculator={calculator}
+                />
             </div>
         );
     }
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,52 +1,24 @@
 import React, { Component } from 'react';
 
-import store from './store';
-import rand from './rand';
-
 class Calculator extends Component {
-    constructor(props) {
-        super(props);
-
-        this.handleSubtractClick = this.handleSubtractClick.bind(this);
-        this.handleSumClick = this.handleSumClick.bind(this);
-
-        store.subscribe(() => {
-            window.console.log('state', store.getState().calculator);
-            this.forceUpdate();
-        });
-    }
-
-    handleSumClick() {
-        store.dispatch({
-            type: 'SUM',
-            value: rand(1, 100),
-        });
-    }
-    handleSubtractClick() {
-        store.dispatch({
-            type: 'SUBTRACT',
-            value: rand(1, 100),
-        });
-    }
-
     render() {
-        const state = store.getState().calculator;
+        const { handleSubtractClick, handleSumClick, calculator } = this.props;
         return (
             <React.Fragment>
                 <h3>
-                    Starting number: <strong>{state.initialValue}</strong>
+                    Starting number: <strong>{calculator.initialValue}</strong>
                 </h3>
                 <h1>
-                    Current sum: <strong>{state.currentValue}</strong>
+                    Current sum: <strong>{calculator.currentValue}</strong>
                 </h1>
                 <h5>
-                    This is the operation number <strong>{state.numberOfOperations}</strong>
+                    This is the operation number <strong>{calculator.numberOfOperations}</strong>
                 </h5>
                 <div>
-                    <button onClick={this.handleSumClick}>Sum a random number</button>
+                    <button onClick={handleSumClick}>Sum a random number</button>
                 </div>
                 <div>
-                    <button onClick={this.handleSubtractClick}>Subtract a random number</button>
+                    <button onClick={handleSubtractClick}>Subtract a random number</button>
                 </div>
             </React.Fragment>
         );
